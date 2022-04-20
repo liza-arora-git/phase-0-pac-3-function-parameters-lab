3402367214cf702ed34cd9e18f3f81103d40576c
Liza Arora <liza.arora@outlook.com>
2022-04-21 08:51:02 +1000

    Function Parameters Lab Complete Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function introduction (name) {
+    //console.log(`Hi, my name is ${name}.`);
+    return `Hi, my name is ${name}.`;
+}
+
+console.log(introduction('Aki'));
+
+function introductionWithLanguage (name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+console.log(introductionWithLanguage('Aki','Ember.js'));
+
+
+function introductionWithLanguageOptional (name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+console.log(introductionWithLanguageOptional('Gracie'));
+console.log(introductionWithLanguageOptional('Gracie','Python'));
